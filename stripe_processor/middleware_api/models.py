9e6f4f1e30579d5acbc10d92e738a1053949de4c
luchor27 <luchoestebanra@gmail.com>
2022-02-10 01:22:38 -0600

    Set transaction token and card data to request serializer and send the response back of the charge

--- stripe_processor/middleware_api/models.py
+++ stripe_processor/middleware_api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Request(models.Model):
-    cc_num = models.CharField(max_length=12)
+    cc_num = models.CharField(max_length=16)
     cvv = models.IntegerField(validators=[MaxValueValidator(9999)])
     exp_month = models.IntegerField(validators=[MaxValueValidator(12)])
-    exp_year = models.IntegerField(validators=[MaxValueValidator(99)])
+    exp_year = models.IntegerField(validators=[MaxValueValidator(9999)])
     trans_id = models.IntegerField()
--- stripe_processor/middleware_api/views.py
+++ stripe_processor/middleware_api/views.py
@@ -1,7 +1,10 @@
+import random
+
 import stripe
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from config import STRIPE_SECRET_KEY
 from middleware_api.serializers import RequestSerializer
@@ -14,7 +17,32 @@ class ChargeViewSet(viewsets.ViewSet):
     A viewset for sending and logging charges
     """
     serializer_class = RequestSerializer
+    api_url = 'https://api.stripe.com/v1/'
 
     def create(self, request):
         data = request.data.copy()
-        print(request)
+        try:
+            exp_date = data.pop('exp_date')
+            data['exp_month'] = int(exp_date[:2])
+            data['exp_year'] = 2000+int(exp_date[2:])
+
+            serializer = RequestSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                token = stripe.Token.create(
+                    card={
+                        "number": serializer.validated_data['cc_num'],
+                        "exp_month": serializer.validated_data['exp_month'],
+                        "exp_year": serializer.validated_data['exp_year'],
+                        "cvc": serializer.validated_data['cvv'],
+                    },
+                )
+                charge_response = stripe.Charge.create(
+                    amount=random.randint(0, 1000),
+                    currency="usd",
+                    source=token.id,
+                )
+                return Response(charge_response)
+            else:
+                return Response(serializer.errors)
+        except Exception as e:
+            return Response(e.error)
